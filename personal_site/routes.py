26b849ca4aa9496c4428b9f88f4d0265bf4345e0
Evan Pollack <evanmpollack@gmail.com>
2022-04-06 21:57:14 -0400

    index, about, exper, edu, proj

--- personal_site/routes.py
+++ personal_site/routes.py
@@ -4,9 +4,26 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
-    return 'Hello, World!!!'
+    return render_template('index')
 
-@app.route('/test')
-def render():
-    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about')
+
+
+@app.route('/experience')
+def experience():
+    return render_template('experience')
+
+
+@app.route('/education')
+def education():
+    return render_template('education')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects')
